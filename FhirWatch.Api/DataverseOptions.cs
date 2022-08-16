ee97da2e18da0ae01c6405b41c90bb08279806fe
Phil Jirsa <phil.jirsa@microsoft.com>
2022-08-16 12:11:38 -0500

    add startup config

---
+++ FhirWatch.Api/DataverseOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace FhirWatch.Api
+{
+    public class DataverseOptions
+    {
+        public string RootUri { get; set; }
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+    }
+}
---
+++ FhirWatch.Api/Startup.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.PowerPlatform.Dataverse.Client;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+[assembly: FunctionsStartup(typeof(FhirWatch.Api.Startup))]
+
+namespace FhirWatch.Api
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void ConfigureAppConfiguration(IFunctionsConfigurationBuilder builder)
+        {
+            FunctionsHostBuilderContext context = builder.GetContext();
+
+            builder.ConfigurationBuilder
+                .AddJsonFile(Path.Combine(context.ApplicationRootPath, "appsettings.json"), optional: true, reloadOnChange: false)
+                .AddJsonFile(Path.Combine(context.ApplicationRootPath, $"appsettings.{context.EnvironmentName}.json"), optional: true, reloadOnChange: false)
+                .AddEnvironmentVariables();
+        }
+
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddScoped(o =>
+            {
+                var config = o.GetService<IConfiguration>();
+                var options = new DataverseOptions();
+                config.GetSection("Dataverse").Bind(options);
+
+                return new ServiceClient($@"AuthType=ClientSecret;url={options.RootUri};ClientId={options.ClientId};ClientSecret={options.ClientSecret}");
+            });
+        }
+    }
+}
